bdd239e64f2941c98ff7ceab531926dde253562e
Kotaro Kamashima <kotaro0726@gmail.com>
2022-09-24 16:12:35 +0900

    Update: Add test scripts

---
+++ test/json_server_get_test.sh
@@ -0,0 +1,3 @@
+curl -X GET localhost:8080 -d '{"offset": 0}'
+curl -X GET localhost:8080 -d '{"offset": 1}'
+curl -X GET localhost:8080 -d '{"offset": 2}'
---
+++ test/json_server_post_test.sh
@@ -0,0 +1,3 @@
+curl -X POST localhost:8080 -d '{"record": {"value": "TGV0J3MgR28gIzEK"}}'
+curl -X POST localhost:8080 -d '{"record": {"value": "TGV0J3MgR28gIzIK"}}'
+curl -X POST localhost:8080 -d '{"record": {"value": "TGV0J3MgR28gIzMK"}}'
