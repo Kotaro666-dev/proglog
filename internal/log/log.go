590a1c7824bfd52d8903d7bc1386497a631b82fc
Kotaro Kamashima <kotaro0726@gmail.com>
2022-09-29 09:07:07 +0900

    Update: Finish log file

--- internal/log/log.go
+++ internal/log/log.go
@@ -1,6 +1,9 @@
 package log
 
 import (
+	"fmt"
+	api "github/Kotaro666-dev/prolog/api/v1"
+	"io"
 	"os"
 	"path"
 	"sort"
@@ -17,6 +20,11 @@ type Log struct {
 
 	activeSegment *segment
 	segments      []*segment
+}
+
+type originReader struct {
+	*store
+	offset int64
 }
 
 func NewLog(dir string, config Config) (*Log, error) {
@@ -69,6 +77,133 @@ func (log *Log) setup() error {
 		}
 	}
 	return err
+}
+
+func (log *Log) Append(record *api.Record) (uint64, error) {
+	log.mu.Lock()
+	defer log.mu.Unlock()
+
+	highestOffset, err := log.highestOffset()
+	if err != nil {
+		return 0, err
+	}
+
+	if log.activeSegment.IsMaxed() {
+		err = log.newSegment(highestOffset + 1)
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	offset, err := log.activeSegment.Append(record)
+	if err != nil {
+		return 0, err
+	}
+	return offset, err
+}
+
+func (log *Log) Read(offset uint64) (*api.Record, error) {
+	log.mu.RLock()
+	defer log.mu.RUnlock()
+
+	var s *segment
+	for _, segment := range log.segments {
+		if segment.baseOffset <= offset && offset < segment.nextOffset {
+			s = segment
+			break
+		}
+	}
+	if s == nil || s.nextOffset <= offset {
+		return nil, fmt.Errorf("offset out of range: %d", offset)
+	}
+	return s.Read(offset)
+}
+
+// Close セグメントをすべてクローズします
+func (log *Log) Close() error {
+	log.mu.Lock()
+	defer log.mu.Unlock()
+
+	for _, segment := range log.segments {
+		if err := segment.Close(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Remove ログをクローズして、そのデータを削除します
+func (log *Log) Remove() error {
+	if err := log.Close(); err != nil {
+		return err
+	}
+	return os.RemoveAll(log.Dir)
+}
+
+// Reset ログを削除して、置き換える新たなログを作成します
+func (log *Log) Reset() error {
+	if err := log.Remove(); err != nil {
+		return err
+	}
+	return log.setup()
+}
+
+func (log *Log) LowestOffset() (uint64, error) {
+	log.mu.Lock()
+	defer log.mu.Unlock()
+	return log.segments[0].baseOffset, nil
+}
+
+func (log *Log) HighestOffset() (uint64, error) {
+	log.mu.Lock()
+	defer log.mu.Unlock()
+	return log.HighestOffset()
+}
+
+func (log *Log) highestOffset() (uint64, error) {
+	offset := log.segments[len(log.segments)-1].nextOffset
+	if offset == 0 {
+		return 0, nil
+	}
+	return offset - 1, nil
+}
+
+// Truncate 最大オフセットがlowestよりも小さいセグメントをすべて削除します
+// ディスク容量は無限ではないため、定期的にTruncateを呼んで、それまでに処理したデータで不要になった古いセグメントを削除します
+func (log *Log) Truncate(lowest uint64) error {
+	log.mu.Lock()
+	defer log.mu.Unlock()
+
+	var segments []*segment
+	for _, segment := range log.segments {
+		if segment.nextOffset <= lowest+1 {
+			if err := segment.Remove(); err != nil {
+				return err
+			}
+			continue
+		}
+		segments = append(segments, segment)
+	}
+	log.segments = segments
+	return nil
+}
+
+// Reader 合意形成の連携を実装し、スナップショットをサポートし、ログの復旧をサポートする必要がある場合、この機能が必要
+func (log *Log) Reader() io.Reader {
+	log.mu.RLock()
+	defer log.mu.RUnlock()
+
+	readers := make([]io.Reader, len(log.segments))
+	for i, segment := range log.segments {
+		readers[i] = &originReader{segment.store, 0}
+	}
+	return io.MultiReader(readers...)
+}
+
+func (originReader *originReader) Read(position []byte) (int, error) {
+	n, err := originReader.ReadAt(position, originReader.offset)
+	originReader.offset += int64(n)
+	return n, err
 }
 
 func (log *Log) newSegment(offset uint64) error {
