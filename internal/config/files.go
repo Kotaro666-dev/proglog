18bf5a374931b09cb21b47e937c0ee7d623cb308
Kotaro Kamashima <kotaro0726@gmail.com>
2022-10-04 08:23:12 +0900

    Update: Add files
    Enable to access to certificate related files

---
+++ internal/config/files.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"os"
+	"path/filepath"
+)
+
+/// 証明書ファイルを簡単に参照できるように用意している
+var (
+	CAFile         = configFile("ca.pem")
+	ServerCertFile = configFile("server.pem")
+	ServerLKeyFile = configFile("server-key.pem")
+)
+
+func configFile(filename string) string {
+	if dir := os.Getenv("CONFIG_DIR"); dir != "" {
+		return filepath.Join(dir, filename)
+	}
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+	return filepath.Join(homeDir, ".proglog", filename)
+}
