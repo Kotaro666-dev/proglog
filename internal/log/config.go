422635485a9844a82c7835e8b1fd2cf5d42d08ad
Kotaro Kamashima <kotaro0726@gmail.com>
2022-10-12 20:48:25 +0900

    Fix: Fix typo

--- internal/log/config.go
+++ internal/log/config.go
@@ -5,7 +5,7 @@ import "github.com/hashicorp/raft"
 type Config struct {
 	Raft struct {
 		raft.Config
-		StreamLayer *raft.StreamLayer
+		StreamLayer *StreamLayer
 		Bootstrap   bool
 	}
 	Segment struct {
