4fbc6ce958317c2df092977d4f020b64e6bf4dfd
Kotaro Kamashima <kotaro0726@gmail.com>
2022-10-19 23:05:00 +0900

    Update: Add setupConfig and run methods

--- cmd/proglog/main.go
+++ cmd/proglog/main.go
@@ -7,7 +7,9 @@ import (
 	"github/Kotaro666-dev/prolog/internal/config"
 	"log"
 	"os"
+	"os/signal"
 	"path"
+	"syscall"
 )
 
 type cli struct {
@@ -16,7 +18,7 @@ type cli struct {
 
 type cfg struct {
 	agent.Config
-	ServerTSLConfig config.TLSConfig
+	ServerTLSConfig config.TLSConfig
 	PeerTLSConfig   config.TLSConfig
 }
 
@@ -67,4 +69,66 @@ func setupFlags(cmd *cobra.Command) error {
 	cmd.Flags().String("peer-tls-ca-file", "", "Path to peer certificate authority.")
 
 	return viper.BindPFlags(cmd.Flags())
+}
+
+/// 設定を読み込んで、エージェントの準備をする
+func (cli *cli) setupConfig(cmd *cobra.Command, args []string) error {
+	configFile, err := cmd.Flags().GetString("config-file")
+	if err != nil {
+		return err
+	}
+	viper.SetConfigFile(configFile)
+
+	if err = viper.ReadInConfig(); err != nil {
+		// 設定ファイルは、存在しなくても問題ない
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return err
+		}
+	}
+
+	cli.cfg.DataDir = viper.GetString("data-dir")
+	cli.cfg.NodeName = viper.GetString("node-name")
+	cli.cfg.BindAddress = viper.GetString("bind-addr")
+	cli.cfg.RPCPort = viper.GetInt("rpc-port")
+	cli.cfg.StartJoinAddress = viper.GetStringSlice("start-join-addrs")
+	cli.cfg.Bootstrap = viper.GetBool("bootstrap")
+	cli.cfg.ACLModelFile = viper.GetString("acl-mode-file")
+	cli.cfg.ACLPolicyFile = viper.GetString("acl-policy-file")
+	cli.cfg.ServerTLSConfig.CertFile = viper.GetString("server-tls-cert-file")
+	cli.cfg.ServerTLSConfig.KeyFile = viper.GetString("server-tls-key-file")
+	cli.cfg.ServerTLSConfig.CAFile = viper.GetString("server-tls-ca-file")
+	cli.cfg.PeerTLSConfig.CertFile = viper.GetString("peer-tls-cert-file")
+	cli.cfg.PeerTLSConfig.KeyFile = viper.GetString("peer-tls-key-file")
+	cli.cfg.PeerTLSConfig.CAFile = viper.GetString("peer-tls-ca-file")
+
+	if cli.cfg.ServerTLSConfig.CertFile != "" && cli.cfg.ServerTLSConfig.KeyFile != "" {
+		cli.cfg.ServerTLSConfig.Server = true
+		cli.cfg.Config.ServerTLSConfig, err = config.SetupTLSConfig(
+			cli.cfg.ServerTLSConfig)
+		if err != nil {
+			return err
+		}
+	}
+
+	if cli.cfg.PeerTLSConfig.CertFile != "" && cli.cfg.PeerTLSConfig.KeyFile != "" {
+		cli.cfg.Config.PeerTLSConfig, err = config.SetupTLSConfig(cli.cfg.PeerTLSConfig)
+		if err != nil {
+			return err
+		}
+	}
+	return err
+}
+
+func (cli *cli) run(cmd *cobra.Command, args []string) error {
+	/// エージェントの作成
+	agent, err := agent.New(cli.cfg.Config)
+	if err != nil {
+		return err
+	}
+	signalChannel := make(chan os.Signal, 1)
+	/// OSからのシグナル処理
+	signal.Notify(signalChannel, syscall.SIGINT, syscall.SIGTERM)
+	<-signalChannel
+	/// OSがプログラムを終了させる場合、エージェントをグレースフルにシャットダウンする
+	return agent.Shutdown()
 }
